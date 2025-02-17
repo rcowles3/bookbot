ebc2eb58b8c5c1d73c5cf6442787292c0adf9094
rcowles3 <ryan.cowles3@gmail.com>
2025-02-16 21:45:29 -0700

    added count_characters

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ def main():
   book_path = "books/frankenstein.txt"
   book_contents = read_book(book_path)
   book_words = count_words(book_contents)
+  book_characters = count_characters(book_contents)
 
-  print(book_words)
+  print(book_characters)
 
 # Returns the contents of a given book
 def read_book(book_path):
@@ -14,6 +15,17 @@ def read_book(book_path):
 # Returns the number of words in a given book
 def count_words(book_contents):
   return len(book_contents.split())
+
+# Count how many times characters are found in book
+def count_characters(book_contents):
+  characters = {}
+  for word in book_contents:
+    lower = word.lower()
+    if lower in characters:
+      characters[lower] += 1
+    else:
+      characters[lower] = 1
+  return characters
 
 # Call Main
 main()
